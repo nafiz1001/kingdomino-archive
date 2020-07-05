03bbdf82e545488087a7be7108b7f96440458f15
Nafiz Islam <nafiz.islam1001@gmail.com>
2020-07-05 16:17:11 -0400

    create ChooseNextDominoStepDefinitions.java

---
+++ src/test/java/kingdomino/features/ChooseNextDominoStepDefinitions.java
@@ -0,0 +1,55 @@
+package kingdomino.features;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ChooseNextDominoStepDefinitions {
+    @Given("the game is initialized for choose next domino")
+    public void the_game_is_initialized_for_choose_next_domino() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the next draft is sorted with dominoes {string}")
+    public void the_next_draft_is_sorted_with_dominoes(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("player's domino selection {string}")
+    public void player_s_domino_selection(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the current player is {string}")
+    public void the_current_player_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("current player chooses to place king on {int}")
+    public void current_player_chooses_to_place_king_on(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("current player king now is on {string}")
+    public void current_player_king_now_is_on(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the selection for next draft is now equal to {string}")
+    public void the_selection_for_next_draft_is_now_equal_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the selection for the next draft selection is still {string}")
+    public void the_selection_for_the_next_draft_selection_is_still(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
