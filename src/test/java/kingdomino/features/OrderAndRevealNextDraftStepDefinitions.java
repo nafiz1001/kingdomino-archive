8bbcb5b0b060c42ade2d841c5728312144f8e161
Nafiz Islam <nafiz.islam1001@gmail.com>
2020-07-05 13:42:02 -0400

    create OrderAndRevealNextDraftStepDefinitions.java

---
+++ src/test/java/kingdomino/features/OrderAndRevealNextDraftStepDefinitions.java
@@ -0,0 +1,67 @@
+package kingdomino.features;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class OrderAndRevealNextDraftStepDefinitions {
+    @Given("the game is initialized for order next draft of dominoes")
+    public void the_game_is_initialized_for_order_next_draft_of_dominoes() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the next draft is {string}")
+    public void the_next_draft_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the dominoes in next draft are facing down")
+    public void the_dominoes_in_next_draft_are_facing_down() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the ordering of the dominoes in the next draft is initiated")
+    public void the_ordering_of_the_dominoes_in_the_next_draft_is_initiated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the status of the next draft is sorted")
+    public void the_status_of_the_next_draft_is_sorted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the order of dominoes in the draft will be {string}")
+    public void the_order_of_dominoes_in_the_draft_will_be(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the game is initialized for reveal next draft of dominoes")
+    public void the_game_is_initialized_for_reveal_next_draft_of_dominoes() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the dominoes in next draft are sorted")
+    public void the_dominoes_in_next_draft_are_sorted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the revealing of the dominoes in the next draft is initiated")
+    public void the_revealing_of_the_dominoes_in_the_next_draft_is_initiated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the status of the next draft is face up")
+    public void the_status_of_the_next_draft_is_face_up() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
