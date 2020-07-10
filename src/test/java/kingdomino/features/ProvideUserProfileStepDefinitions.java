8779470847691f0697cfc3bd6330149db5698b3f
nafiz1001 <nafiz.islam1001@gmail.com>
2020-07-10 15:38:19 -0400

    create ProvideUserProfileStepDefinitions.java

---
+++ src/test/java/kingdomino/features/ProvideUserProfileStepDefinitions.java
@@ -0,0 +1,97 @@
+package kingdomino.features;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProvideUserProfileStepDefinitions {
+    @Given("the program is started and ready for providing user profile")
+    public void the_program_is_started_and_ready_for_providing_user_profile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("there are no users exist")
+    public void there_are_no_users_exist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I provide my username {string} and initiate creating a new user")
+    public void i_provide_my_username_and_initiate_creating_a_new_user(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user {string} shall be in the list of users")
+    public void the_user_shall_be_in_the_list_of_users(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the following users exist:")
+    public void the_following_users_exist(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user creation shall {string}")
+    public void the_user_creation_shall(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I initiate the browsing of all users")
+    public void i_initiate_the_browsing_of_all_users() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the users in the list shall be in the following alphabetical order:")
+    public void the_users_in_the_list_shall_be_in_the_following_alphabetical_order(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the following users exist with their game statistics:")
+    public void the_following_users_exist_with_their_game_statistics(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I initiate querying the game statistics for a user {string}")
+    public void i_initiate_querying_the_game_statistics_for_a_user(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the number of games played by {string} shall be {int}")
+    public void the_number_of_games_played_by_shall_be(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the number of games won by {string} shall be {int}")
+    public void the_number_of_games_won_by_shall_be(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
